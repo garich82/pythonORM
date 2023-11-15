81ef42d1902c8c4b4bd44a2756f957cd6305b3c4
garich82 <garo.odabashyan@protonmail.com>
2023-11-15 17:31:33 +0000

    Added first problem

--- orm_skeleton_lab_9/main_app/models.py
+++ orm_skeleton_lab_9/main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from main_app.managers import ProductManager
 
 
 class Category(models.Model):
@@ -11,6 +12,8 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     is_available = models.BooleanField(default=True)
+
+    objects = ProductManager()
 
     def __str__(self):
         return f"{self.category.name}: {self.name}"
